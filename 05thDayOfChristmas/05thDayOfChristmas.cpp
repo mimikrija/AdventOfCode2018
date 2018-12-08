c3096bb5aa32abf3e7b062e77e847723a332748a
mimikrija <maja.gacesa@gmail.com>
2018-12-08 21:17:53 +0100

    second part working

--- 05thDayOfChristmas/05thDayOfChristmas.cpp
+++ 05thDayOfChristmas/05thDayOfChristmas.cpp
@@ -15,23 +15,9 @@ bool upperlower(char first, char second)
 	return ( abs(int(first) - int(second)) == 32 );
 }
 
-
-int main()
+int ResultLength (string Sequence)
 {
-	string Sequence, InputFileName, OriginalSequence;
-
-
-	cout << "Input File name? " << endl;
-	cin >> InputFileName;
-	ifstream InputFile;
-
-	InputFile.open(InputFileName);
-	Sequence = { istream_iterator<char>{InputFile},{} };
- 
-
-
-
-int it=0;
+    int it=0;
 
 while ( it < Sequence.length() )
 {
@@ -39,6 +25,7 @@ while ( it < Sequence.length() )
 	    if ( upperlower(Sequence[it],Sequence[it+1]) )
 		{
 		    Sequence.erase(it,2);
+		    //cout << "Sequence length is now: " << Sequence.length() << endl;
 		    if ( it>0 )
 		    {
 		        it--;
@@ -55,11 +42,53 @@ while ( it < Sequence.length() )
 		
 	
 }
+return Sequence.length();
+}
 
-	cout << " Resulting sequence length is: " << Sequence.length() <<"!!" << endl;
-	
-	
 
+
+int main()
+{
+	string Sequence, InputFileName;
+
+
+	cout << "Input File name? " << endl;
+	cin >> InputFileName;
+	ifstream InputFile;
+
+	InputFile.open(InputFileName);
+	Sequence = { istream_iterator<char>{InputFile},{} };
+
+
+
+// first part
+
+	cout << " Resulting sequence length is: " << ResultLength(Sequence) <<"!!" << endl;
+	
+// second part
+	int OptimalSize = Sequence.length();
+	
+	for ( int c=65; c <= 90; c++)
+	{
+	    string ShorterSequence = Sequence;
+	    int newsize = Sequence.length() - count_if( begin(Sequence),end(Sequence), [c](char elem) { return ( int(elem)==c || int(elem)==c+32 );});
+	    remove_if( begin(ShorterSequence),end(ShorterSequence), [c](char elem) { return ( int(elem)==c || int(elem)==c+32 );});
+	    ShorterSequence.resize(newsize);
+	    
+	    int temp = ResultLength(ShorterSequence);
+	    
+	    //cout << " Resulting sequence length is, if we remove letter " << char(c) << " is: " << temp <<"!!" << endl;
+	    
+	    if ( temp < OptimalSize )
+	    {
+	        OptimalSize = temp;
+	    }
+	   
+	    
+	    
+	}
+	
+	cout << "Optimal sequence length is " << OptimalSize << "!!" << endl;
 
 	return 0;
 
