dd5bb89b14e04c1c5909a9157ede47322d7407bb
Maja Gacesa <maja.gacesa@ansys.com>
2021-03-27 15:12:36 +0100

    [day05] part 2 - test for all letters

--- python/05.py
+++ python/05.py
@@ -23,7 +23,17 @@ def reduce_molecule(in_molecule):
     return len(reduced)
 
 
+def remove_type(in_molecule, type_ord):
+    removed = ''.join(c for c in in_molecule if ord(c) != type_ord and ord(c) != type_ord + 32)
+    return deque(removed)
+
+
+def find_shortest_reduction(in_molecule):
+    return min(reduce_molecule(remove_type(in_molecule, type_ord)) for type_ord in range(ord('A'), ord('Z')+1))
+
 
 original_molecule = deque(get_input('inputs/05.txt')[0])
 
-print_solutions(reduce_molecule(original_molecule))
+print_solutions(reduce_molecule(original_molecule), find_shortest_reduction(original_molecule))
+# Part 1 solution is: 11118
+# Part 2 solution is: 6948
