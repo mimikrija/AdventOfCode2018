fc2e62dcb2bf870ada8d070091c6581ad3097ee4
Maja Gacesa <maja.gacesa@ansys.com>
2021-03-27 14:22:43 +0100

    [day04] party 2

--- python/04.py
+++ python/04.py
@@ -27,5 +27,13 @@ sleepy_minutes = {ID: len(minutes) for ID, minutes in guard_sleepy_minutes.items
 sleepiest_guard = max(sleepy_minutes, key=sleepy_minutes.get)
 sleepiest_minute = Counter(guard_sleepy_minutes[sleepiest_guard]).most_common(1)[0][0]
 
-part_1 = sleepiest_guard * sleepiest_minute
-print_solutions(part_1)
+minute_frequency = {ID: tuple(reversed(list(Counter(guard_sleepy_minutes[ID]).most_common(1)[0]))) for ID in guard_sleepy_minutes.keys()}
+most_frequently_asleep_on_a_minute_guard = max(minute_frequency, key=minute_frequency.get)
+most_frequent_minute = minute_frequency[most_frequently_asleep_on_a_minute_guard][1]
+
+strategy_1 = sleepiest_guard * sleepiest_minute
+strategy_2 = most_frequently_asleep_on_a_minute_guard * most_frequent_minute
+
+print_solutions(strategy_1, strategy_2)
+# Part 1 solution is: 84834
+# Part 2 solution is: 53427
