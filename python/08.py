fca5fd7fbe89ed9d79840765c84745a1fa817657
Maja Gacesa <maja.gacesa@ansys.com>
2021-04-12 17:25:07 +0200

    [day08] improve dfs by removing redundant branches

--- python/08.py
+++ python/08.py
@@ -8,23 +8,21 @@ input_file = deque(int(num) for num in get_input('inputs/08.txt', ' '))
 
 
 def dfs(input_stream, metadata=0):
-    if input_stream:
-        children_count = input_stream.popleft()
-        metadata_count = input_stream.popleft()
-        # base condition: single child
-        if children_count == 0:
-            metadata += sum(input_stream.popleft() for _ in range(metadata_count))
-            # return reduced input stream
-            return input_stream, metadata
-        else:
-            for child in range(children_count):
-                rest, metadata = dfs(input_stream, metadata)
-            # once we clear all the children, we need to append the metadata of the parent:
-            rest.appendleft(metadata_count)
-            rest.appendleft(0)
-            return dfs(rest, metadata)
-    else:
+    children_count = input_stream.popleft()
+    metadata_count = input_stream.popleft()
+    # base condition: single child
+    if children_count == 0:
+        metadata += sum(input_stream.popleft() for _ in range(metadata_count))
+        # return reduced input stream
         return input_stream, metadata
+    else:
+        for child in range(children_count):
+            rest, metadata = dfs(input_stream, metadata)
+        # once we clear all the children, we need to append the metadata of the parent:
+        rest.appendleft(metadata_count)
+        rest.appendleft(0)
+        return dfs(rest, metadata)
+
 
 
 _, part_1 = dfs(input_file)
